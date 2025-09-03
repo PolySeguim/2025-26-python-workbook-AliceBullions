666ab216a4dbb5b76a4910c58af68051c272381f
AliceBullions <soccertodance@gmail.com>
2025-09-03 15:03:59 +0000

    9/3/25

--- ex1-5.py
+++ ex1-5.py
@@ -6,24 +6,50 @@ address formatted in the manner that you would usually see it
 on the outside of an envelope.  Your program does not need to 
 read any input from the user.  (9 lines)
 """
-
+def mailingAddress():
+    #create variables for all the information 
+    name = input("What is your name? ")
+    #address
+    #city
+    #state
+    #zip
+    address = input("What is your address?")
+    city = input("What is your city?")
+    state = input("What is your state?")
+    zip = input("What is your zip?")
+    print(name)
+    print(address)
+    print(city,",", state, zip)
 """
 Exercise 2:  Hello
 Write a program that asks the user to enter his or her name.  
 The program should respond with a message that says hello to 
 the user, using his or her name.  (9 lines)
 """
+def HelloName():
+    name = input("What is your name?")
+    print("hello", name)
 
 """
 Exercise 3:  Area of a Room
 Write a program that asks the user to enter the width and 
-length or a room.  Once the values have been read, your 
+length or a room. Once the values have been read, your 
 program should compute and display the area of the room.  
 The length and the width will be entered as floating point 
 numbers.  Include units in your prompt and output message;  
 either feet or meters, depending on which unit you are more 
 comfortable working with.  (13 lines)
 """
+"""
+def AreaRoom():
+    print("area of room")
+    units = input("what units are you using? feet or meter:")
+    width = float(input("width:"))
+    length = float(input("length:"))
+    AreaOfRoom = width*length
+    print(AreaOfRoom, units)
+"""
+
 
 """
 Exercise 4:  Area of a Field
@@ -31,6 +57,16 @@ Create a program that reads the length and width of a
 farmer’s field from the user in feet.  Display the 
 area of the field in acres.  
 Hint: There are 43,560 square feet in an acre
+"""
+"""
+def FarmersField():
+    print("area of a farmer's field")
+    width = float(input("width:"))
+    length = float(input("length:"))
+    AreaOfField = width*length/43560
+    print("the are of the farmers field is", AreaOfField, "acres")
+
+FarmersField();    
 """
 
 """
@@ -47,10 +83,18 @@ those containers.  Format the output so that it includes a dollar
 sign and always displays exactly two decimal places.  (15 lines)
 """
 
+def Refund():
+    TenCentDeposits = float(input("how many bottles are less than or equal to 1 liter?"))
+    TwentyFiveCentDeposits = float(input("how many bottles are more than 1 liter?"))
+    OneLiterOrLessBottles = float(TenCentDeposits*0.10)
+    MoreThanOneLiterBottles = float(TwentyFiveCentDeposits*0.25)
+    RefundPrice = OneLiterOrLessBottles+MoreThanOneLiterBottles
+    print("$",RefundPrice)
+Refund();
 
-def print_hi(name):
-    print(f"Hi, {name}")
 
 
-if __name__ == "__main__":
-    print_hi("Netbeans")
+
+
+
+
--- notes.py
+++ notes.py
@@ -12,3 +12,114 @@ def whatType(userInput):
 multiple line comments 
 """
 #Call the function with different inputs 
+#If you dont call the functions, nothing will run in the program 
+
+"""
+Test SUITE
+"""
+whatType(3)
+whatType(3.0)
+whatType("3.0")
+whatType(True)
+whatType("Alice")
+whatType('p')
+
+#create a variable named message 
+message = """this is a 
+multiline message 
+to my bestie."""
+print(message)
+
+#test inputs to print and see how they print 
+print(42000)
+#every time you have a comma between values, it will understand it as a different 
+#parameter input 
+print(42, "poly", 3, "chem", "computer")
+print(42000)
+print(42,000)
+
+name = "polyana"
+newName = "poly"
+name = newName
+newName = name 
+
+print(name)
+print(newName)
+
+
+classOf2026 = ("Student 1", "Student 2")
+#seinors = not a good variable and name because it is not specific 
+
+
+#MLA formatting for GEEKS
+#Global varibale for things that can not change 
+
+#In naming varibale the variable day <> Day 
+#we want to use lower case as much as possible
+#for Python day_of_the_week
+#in Java dayOfTheWeek 
+
+"""
+ILLEGAL VARIABLE NAMES!
+21yeasold ="party!"
+dolla$$$$
+def = "def"
+class = python 
+"""
+
+"""
+Some kinds of statements are:
+-While statements 
+-for statements 
+-import statements 
+-if statemnts 
+-and so on...
+"""
+print(2+2)
+print("hello")
+print(len("hello"))
+#len will give you the length of the keyword 
+
+print(20+12)
+hour = 1
+print(hour - 1)
+print(hour * 60, "minutes in", hour, "hour")
+
+myName = "alice" 
+print(myName * 5)
+# str is casting the integer date type to a string 
+# so you can cocatenate (add) two strings together 
+print(myName + str(5))
+
+#Operations
+# Addition 
+#  add numbers or concatenate string 
+# Subtraction
+#  to numbers only 
+# Division 
+#  numbers only 
+#  -/- division (typical) but the answer is always a float 
+#  -//- floor division (divides to the largest integer) answer in an int 
+#  -%- modulous operator (finds the remainder of the division) answer is an int 
+print(10/3) #3.333333
+print(10//3) #3
+print(10%3) #1
+# multipication 
+#  for numbers and strings 
+#  -*- multiplies only 
+#  -**- 
+print(8*2)
+print(8**2)
+
+#always round down 
+print(int(3.14))
+print(int("1977"))
+print(int(-4.11212132))
+print(int(-4.9999))
+#print(int("Alice")) will not work "alice" is not a number 
+    
+print(float(1977))
+print(float(3.1415))
+
+print(str(1977))
+print(str(3.0))
