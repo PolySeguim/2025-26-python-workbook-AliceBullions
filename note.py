2c97dcb5e09cbac792391fad6a75078553c12036
AliceBullions <soccertodance@gmail.com>
2025-09-11 11:08:56 -0400

    09-11

---
+++ note.py
@@ -0,0 +1,14 @@
+# def is the keyword to define a function 
+#what type is the name of the function
+# userInput is the parameter of the function 
+def whatType(userInput):
+    print(type(userInput))
+    # print is a Python built-in fuction that prints to the console 
+    # type is a Python-built in function that finds the data type 
+    # userInput is the variable that the user enters 
+# the pound symbol is for one line comments
+# the program ignores all comments 
+"""
+multiple line comments 
+"""
+#Call the function with different inputs 
